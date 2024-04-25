f6038c6b8b459defbc7877b4d2f04f058ef7bd27
Alden Darefsky <adarefsky@duck.com>
2024-04-25 09:07:02 -0400

    cleaning code

---
+++ target/debug/deps/trender-130c7b3b64caba66.d
@@ -0,0 +1,5 @@
+/home/alden/Code/trender/target/debug/deps/libtrender-130c7b3b64caba66.rmeta: src/lib.rs
+
+/home/alden/Code/trender/target/debug/deps/trender-130c7b3b64caba66.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/trender-4d45e37f26fb88a4.d
@@ -0,0 +1,5 @@
+/home/alden/Code/trender/target/debug/deps/trender-4d45e37f26fb88a4: src/lib.rs
+
+/home/alden/Code/trender/target/debug/deps/trender-4d45e37f26fb88a4.d: src/lib.rs
+
+src/lib.rs:
